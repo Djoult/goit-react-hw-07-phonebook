214119880a68f98a3f0bca65645e0e0ab1b8bc21
Djoult <55453354+Djoult@users.noreply.github.com>
2023-06-23 16:03:22 +0300

    bugfix alert

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,6 @@
 import { Form, Button, Label, Input, Div } from './ContactForm.styled';
-// import { useDispatch, useSelector } from 'react-redux';
-// import shortid from 'shortid';
-// import { addContact } from 'redux/contactsSlice';
-// import { getContacts } from 'redux/selectors';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { useState } from 'react';
 import {
@@ -30,15 +28,15 @@ export const ContactForm = () => {
         contact.number === number
     );
     if (isContact) {
-      alert(`${name} is already in contacts`);
+      toast.info(`${name} is already is contacts`);
       return;
     }
 
     try {
       await addContact(newContact);
-      alert(`Contact ${newContact.name} was added`);
+      toast.success(`${newContact.name} successfully added`);
     } catch (error) {
-      alert('Oops! Something went wrong. Please try again!');
+      toast.error('Oops! Something went wrong. Please try again!');
     }
   };
 
@@ -49,35 +47,38 @@ export const ContactForm = () => {
   };
 
   return (
-    <Div>
-      <Form onSubmit={handleSubmit}>
-        <Label>
-          Name
-          <Input
-            type="text"
-            name="name"
-            pattern="[A-Za-zА-Яа-я\s]+"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </Label>
-        <Label>
-          Phone
-          <Input
-            type="tel"
-            name="number"
-            pattern="\+?[0-9\s\-\(\)]+"
-            value={number}
-            onChange={e => setNumber(e.target.value)}
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </Label>
+    <>
+      <Div>
+        <Form onSubmit={handleSubmit}>
+          <Label>
+            Name
+            <Input
+              type="text"
+              name="name"
+              pattern="[A-Za-zА-Яа-я\s]+"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </Label>
+          <Label>
+            Phone
+            <Input
+              type="tel"
+              name="number"
+              pattern="\+?[0-9\s\-\(\)]+"
+              value={number}
+              onChange={e => setNumber(e.target.value)}
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </Label>
 
-        <Button type="submit">Add contact</Button>
-      </Form>
-    </Div>
+          <Button type="submit">Add contact</Button>
+        </Form>
+      </Div>
+      <ToastContainer />
+    </>
   );
 };
